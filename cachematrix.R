e39f6f67cbe10efa0e5f3b26080f853c5f910038
Graham Stein <s13gram@gmail.com>
2015-09-26 14:17:40 +0700

    comments
    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
 
-## Put comments here that give an overall description of what your
-## functions do
+## This program has two functions. 
+## The first inputs a matrix and stores it. 
+## The second checks for cached data. 
+##If none is found it calculates and outputs the inverse matrix 
 
-## Write a short comment describing this function
+## This function inputs a matrix and stores it.
 
 makeCacheMatrix <- function(x = matrix()) {
   get <- function() input_matrix
@@ -14,7 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function checks for cached data and matrix equality. 
+##If negative, it calculates and outputs the inverse matrix 
 
 cacheSolve <- function(x) {
   ## Return a matrix that is the inverse of 'x'
